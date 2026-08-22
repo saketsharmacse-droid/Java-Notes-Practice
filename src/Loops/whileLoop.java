ec5cfb546d2779133ee33233f9ac5438aec431d1
Saket Sharma <saket.sharma.cse@gmail.com>
2026-08-22 16:46:56 +0530

    learnt about while loops

---
+++ src/Loops/whileLoop.java
@@ -0,0 +1,21 @@
+package Loops;
+
+public class whileLoop {
+    static void main() {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+
+        int i = 1;
+        while(i<=3){
+            System.out.println(i);
+            i++;
+        } //jab tak condition true tab tak execute hoga.
+        //if condition false nhi hoga, tab tak loop execute hote rhega.
+    }
+}
+
+//jab bhi humein cheezein repeat krni hai, we use loops.
+
+//while(1) nhi run hota hai java mei: c++,c mei chal jata hai but not in java
+//white(boolean) hi daalna rhega
