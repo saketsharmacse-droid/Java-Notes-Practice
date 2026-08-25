88afd2726f6b904e0130af02a3d13a7b14de5dfb
Saket Sharma <saket.sharma.cse@gmail.com>
2026-08-25 08:06:51 +0530

    learnt the basics of oops

---
+++ src/OOPS/introduction1.java
@@ -0,0 +1,33 @@
+package OOPS;
+
+public class introduction1 {
+    static void main() {
+
+        System.out.println("OOPS in jva!");
+    }
+}
+
+//oop
+//object oriented programming tries to map the code instructions with the real world, making the code short and easier to understand.
+//solving the problem by creating objects is one of the most popular approaches in programming
+//DRY: do not repeat yourself:code reusability.
+
+//class
+//a class is a blueprint for creating objects.
+//jaise ek form hai, sab log usi form ko use krke apna details bhrenge, toh each student's form is an object of the class form.
+//jaise exam ka answer-sheet hai, all the students will write different answers and different hand writings, but the overall paper is under the class paper.
+//class: contains info to create a valid object: object instantiation to create objects.
+
+//object
+//an object is an instantiation of a class, when a class is defined, a template(info) is defined, memory is allocated only after the object instantiation.
+//object ek real world entity hai, class ka ek instance hai.
+
+//class ko bnane ke liye bhi memory lagti hai thodi si, but jab tak humnein object nhi bnyi tab tak uske liye memory allocate nhi hogi.
+//ek class ke multiple instances bna sakte hai and can use them for different purposes.
+
+//How to model an actual problem in oops?
+//in general terms: we find noun, adjective and verb.
+//noun --> class --> employee
+//adjective --> attributes --> name, age, salary
+//verbs --> methods --> getSalary(), increment()
+
