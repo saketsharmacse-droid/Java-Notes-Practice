fdb9645ae0fff4f3ef817815db0dfeac63fc4e8c
Saket Sharma <saket.sharma.cse@gmail.com>
2026-08-25 08:19:39 +0530

    learnt about the concepts of oop

---
+++ src/OOPS/oops2.java
@@ -0,0 +1,31 @@
+package OOPS;
+
+public class oops2 {
+    static void main() {
+
+    }
+}
+
+//OOPS Terminology:
+//Abstraction --> hiding the unnecessary internal details.
+// (shows only essential info!) : jaise we use phone without bothering about how it is made.
+//we use car without nowing how the brakes work and all
+//aam khaao gutliyan mat gino.
+
+//Encapsulation --> The act of putting various components together(in a capsule).
+//in java,encapsulation means that the sensitive data can be ghidden from the users.
+//laptop is a single entity with wifi+speaker+storage in a single box
+//class is a capsule jisme bahut saari functionality is bundled and can be used as per the requirements.
+//we can explore the code and see the working, but emcapsulation kr ehumare software ka experience bahut accha ho jata hai.
+
+//Inheritance
+//if mereko rikshaw bnana hai, toh i can use the wheels, handes and all, firse nhi bnaunga saare parts baith ke.
+//jaise e rikshaw bnana hai toh i shall use an already available rikshaw and modify it.
+//phone se sart phone bnate time we used the already bna hua phone, usse fir bnayi cheezein.
+//existing cheez ke features ke saath aur additional features daalte hai.
+//implements dry in a best manner
+
+//polymorphism
+//one entity many forms.
+//smartphone can be used as a camers, as a calculator, as a speaker, as a calling device
+
