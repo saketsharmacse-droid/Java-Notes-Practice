011c9337531ead670cd29fade3fd7e635e7379be
Saket Sharma <saket.sharma.cse@gmail.com>
2026-09-15 08:15:31 +0530

    learnt about method overriding

--- src/Inheritance/methodOverriding5.java
+++ src/Inheritance/methodOverriding5.java
@@ -13,7 +13,7 @@ class B extends A{
     public void meth1(){
         System.out.println("I am a method of class B!");
     }
-    @override //its is word that shows us we have overridden.
+//    @override //its is word that shows us we have overridden.
     //yeh override notation nhi likhenge toh nhi koi problem nhi aayega.
     //but we should write this annotation as yeh error deta hai agar hum upar kuch change krenge toh, toh humein pta chlega ki actual mei override hora hai ki nhi hora.
     //suppose kuch changes kiye, and override nhi hora ab, fir bhi humein lag rha ki override hora hai krke, toh this annotation will tell us ki nhi hora krke.
